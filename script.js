d33c7daff5cf0e0e651208afb3672ad0bce80410
SebastianoMV <sebastianovisco@gmail.com>
2022-04-08 14:07:52 +0200

    Milestone 1 Fizz e Buzz in console

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const container= document.querySelector('.container');
 
 let limit = 101;
+const fizz ='Fizz';
+const buzz ='Buzz';
+const fizzbuzz = 'FizzBuzz'
 
 for(let i = 1; i < limit; i++){
   
-  console.log(i);
+  if(i % 3 && i % 5){
+    console.log(i);
+  } else if( i % 3 == 0 && i % 5 == 0 ){
+    console.log('FizzBuzz');
+  }else if(i % 3 == 0){
+    console.log('Fizz');
+  } else if(i % 5 == 0){
+    console.log('Buzz');
+  }
+  
 }
