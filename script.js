bd3e333ab5afeeb76b09f0f6db0deb6a8391507e
SebastianoMV <sebastianovisco@gmail.com>
2022-04-11 09:53:00 +0200

    Avevo lasciato delle const inutili

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const container= document.querySelector('.container');
 
 let limit = 101;
-const fizz ='Fizz';
-const buzz ='Buzz';
-const fizzbuzz = 'FizzBuzz'
 
 for(let i = 1; i < limit; i++){
   const boxEl = document.createElement('div');
@@ -15,15 +12,15 @@ for(let i = 1; i < limit; i++){
     console.log(i);
     boxEl.append(i);
   } else if( i % 3 == 0 && i % 5 == 0 ){
-    console.log('FizzBuzz');
+    
     boxEl.append('FizzBuzz');
     boxEl.classList.add('box-fizzbuzz');
   }else if(i % 3 == 0){
-    console.log('Fizz');
+   
     boxEl.append('Fizz');
     boxEl.classList.add('box-fizz');
   } else if(i % 5 == 0){
-    console.log('Buzz');
+    
     boxEl.append('Buzz');
     boxEl.classList.add('box-buzz');
   }
